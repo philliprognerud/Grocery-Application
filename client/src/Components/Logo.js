840d70aad134ac142f30babe01524393e5f6f7e3
Phillip <philliprognerud2@yahoo.com>
2018-02-28 21:11:25 +0000

    test

--- client/src/Components/Logo.js
+++ client/src/Components/Logo.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react';
 const style = {
     logo: {
         width: "50px",
-        display: "inline-block",
-        marginLeft: "80px",
-        marginTop: "25px"
+        display: "inlineBlock"
     },
     header: {
-        display: "inline-block"
+        display: "inlineBlock"
     }
-}
+};
 
 
 class Logo extends Component {
