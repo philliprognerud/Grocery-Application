6dc35b1b626dad368de7200482e50f591084036a
EC2 Default User <philliprognerud2@yahoo.com>
2018-03-06 21:05:59 +0000

    c9 commit

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,20 +1,16 @@
 /*eslint-disable no-unreachable, no-extra-semi, no-unused-vars, no-undef, unknown-require, forbiddenExportImport, semi, no-const-assign, check-tern-plugin*/
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                <button className="ui primary button">
-                  Save
-                </button>
-                <button className="ui button">
-                  Discard
-                </button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button className="ui primary button">Save</button>
+        <button className="ui button">Discard</button>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
