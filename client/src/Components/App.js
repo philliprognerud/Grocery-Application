be9f072841910586fce7bea3ba62198095fb02df
EC2 Default User <philliprognerud2@yahoo.com>
2018-03-06 21:06:51 +0000

    c9 commit

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,27 +1,27 @@
 /*eslint-disable no-unreachable, no-extra-semi, no-unused-vars, no-undef, unknown-require, forbiddenExportImport, semi, no-const-assign, check-tern-plugin*/
 
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import ShoppingCart from './ShoppingCart';
-import Logo from './Logo'
-import MenuBar from './MenuBar'
-import ActionCard from './ActionCard'
-const Landing = () => <h2>Landing Page</h2>
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import ShoppingCart from "./ShoppingCart";
+import Logo from "./Logo";
+import MenuBar from "./MenuBar";
+import ActionCard from "./ActionCard";
 
 const App = () => {
-    return (
+  return (
+    <div>
+      <BrowserRouter>
         <div>
-            <BrowserRouter>
-                <div>
-                    <Logo />
-                    <ShoppingCart />
-                    <Route exact path="/phil" component={Landing} />
-                    <Route exact path="/shawn/*" component={MenuBar} />
-                   <Route exact path ="/dom/*"  component={ActionCard} />
-                </div>
-            </BrowserRouter>
+          <Logo />
+          <ShoppingCart />
+          <Route exact path="/phil" component={Landing} />
+          <Route exact path="/shawn/*" component={MenuBar} />
+          <Route exact path="/dom/*" component={ActionCard} />
         </div>
-    )
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
